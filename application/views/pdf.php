c7de854b72557f1038a37191cc542a379e823933
Januar Fonti <januarfonti@yahoo.co.id>
2015-10-26 00:05:28 +0700

    html2pdf added

--- application/views/pdf.php
+++ application/views/pdf.php
@@ -1,82 +1,94 @@
+<?php ob_start(); ?>
+
+
+
+
+
+
+
+<h4>LAPORAN SURAT MASUK</h4>
+<h5>BAGIAN ANGGARAN BPKAD KABUPATEN BLITAR</h5>
+
+
+
+            
+                <table border="1px">
+                  <thead>
+                    <tr>
+                      <th rowspan="2">No</th>
+                      <th rowspan="2">No Agenda</th>
+                      <th rowspan="2">No Surat</th>
+                      <th rowspan="2">Pengirim</th>
+                      <th rowspan="2">Tanggal Surat</th>
+                      <th rowspan="2">Tanggal Masuk</th>
+                      <th rowspan="2">Perihal</th>
+                      <th rowspan="2">Isi Singkat</th>
+                      <th rowspan="2">Lampiran</th>
+                      <th rowspan="2">Nominal</th>
+                      <th class="center" colspan="3">Disposisi</th>
+                      <th class="center" colspan="3">Disposisi Kepala BPKAD</th>
+                      <th class="center" colspan="3">Disposisi Kepala Bidang Anggaran</th>
+                      <th class="center" colspan="3">Tindak Lanjut</th>
+                    </tr>
+                    <tr>
+                      <th class="center">Tanggal</th>
+                      <th class="center">Oleh</th>
+                      <th class="center">Isi</th>
+                      <th class="center">Tanggal</th>
+                      <th class="center">Oleh</th>
+                      <th class="center">Isi</th>
+                      <th class="center">Tanggal</th>
+                      <th class="center">Oleh</th>
+                      <th class="center">Isi</th>
+                      <th class="center">Oleh</th>
+                      <th class="center">Tanggal</th>
+                      <th class="center">Hasil</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                  <?php $no = 1; if (isset($laporan)){foreach($laporan as $row){ ?>
+                    <tr>
+                      <td><?php echo $no; ?></td>
+                      <td><?php echo $row->no_agenda; ?></td>
+                      <td><?php echo $row->no_surat; ?></td>
+                      <td><?php echo $row->pengirim; ?></td>
+                      <td><?php echo $row->tgl_surat; ?></td>
+                      <td><?php echo $row->tgl_masuk; ?></td>
+                      <td><?php echo $row->perihal; ?></td>
+                      <td><?php echo $row->uraian_singkat; ?></td>
+                      <td><?php echo $row->upload; ?></td>
+                      <td><?php echo $row->nominal; ?></td>
+                      <td><?php echo $row->tgl_a; ?></td>
+                      <td><?php echo $row->disposisi_a; ?></td>
+                      <td><?php echo $row->isi_disposisi_a; ?></td>
+                      <td><?php echo $row->tgl_b; ?></td>
+                      <td><?php echo $row->disposisi_b; ?></td>
+                      <td><?php echo $row->isi_disposisi_b; ?></td>
+                      <td><?php echo $row->tgl_c; ?></td>
+                      <td><?php echo $row->disposisi_c; ?></td>
+                      <td><?php echo $row->isi_disposisi_c; ?></td>
+                      <td><?php echo $row->pelaksana; ?></td>
+                      <td><?php echo $row->tgl_pelaksana; ?></td>
+                      <td><?php echo $row->keterangan; ?></td>
+                    </tr>
+                    <?php $no++; } } ?>
+                  </tbody>
+                </table>
+
+
+<!--HTML2PDF START-->
 <?php
-class PDF extends FPDF
-{
-	//Page header
-	function Header()
-	{
-                $this->setFont('Arial','',10);
-                $this->setFillColor(255,255,255);
-                //$this->cell(100,6,"Laporan daftar pegawai gubugkoding.com",0,0,'L',1); 
-                $this->cell(100,6,"Printed date : " . date('d/m/Y'),0,1,'R',1); 
-                //$this->Image(base_url().'assets/dist/img/user7-128x128.jpg', 10, 25,'20','20','jpeg');
-                
-                /*
-                $this->Ln(12);
-                $this->setFont('Arial','',14);
-                $this->setFillColor(255,255,255);
-                $this->cell(25,6,'',0,0,'C',0); 
-                $this->cell(1,6,'Laporan daftar pegawai gubugkoding.com',1,0,'L',1); 
-                */
+$filename="coba.pdf"; //Set Nama File
+$content = ob_get_clean();
 
-                $this->SetFont('Times','B',12);
-                $this->Cell(10,11,'No',1,0,'C');
-                $this->Cell(75,11,'MATERI',1,0,'C');
-                $this->Cell(30,6,'SKOR',1,0,'C');
-                $this->Cell(65,6,'NILAI',1,1,'C');
-                $this->SetX(95);
-                $this->SetFont('Times','B',11);
-                $this->Cell(15,5,'Teori',1,0,'C');
-                $this->Cell(15,5,'Praktik',1,0,'C');
-                $this->Cell(15,5,'Teori',1,0,'C');
-                $this->Cell(15,5,'Praktik',1,0,'C');
-                $this->Cell(20,5,'Jumlah',1,0,'C');
-                $this->Cell(15,5,'Total',1,1,'C');
-                $this->SetFont('Times','',11);
-                
-                
-                $this->Ln(5);
-                $this->setFont('Arial','',10);
-                $this->setFillColor(230,230,200);
-                $this->cell(10,6,'No.',1,0,'C',1);
-                $this->cell(105,6,'Nama Lengkap',1,0,'C',1);
-                $this->cell(30,6,'No. HP',1,0,'C',1);
-                $this->cell(50,6,'Jenis Kelamin',1,1,'C',1);
-                
-	}
- 
-	function Content($laporan)
-	{
-            $ya = 46;
-            $rw = 6;
-            $no = 1;
-                foreach ($laporan as $key) {
-                        $this->setFont('Arial','',10);
-                        $this->setFillColor(255,255,255);	
-                        $this->cell(10,10,$no,1,0,'L',1);
-                        $this->cell(105,10,$key->perihal,1,0,'L',1);
-                        $this->cell(30,10,$key->tgl_masuk,1,0,'L',1);
-                        $this->cell(50,10,$key->tgl_surat,1,1,'L',1);
-                        $ya = $ya + $rw;
-                        $no++;
-                }            
+ try
+ {
+  $html2pdf = new HTML2PDF('L','A4','fr','UTF-8');
+  $html2pdf->writeHTML($content);
+  $html2pdf->pdf->includeJS('print(TRUE)');
+  $html2pdf->Output($filename);
+ }
+ catch(HTML2PDF_exception $e) { echo $e; }
+?>
+<!--HTML2PDF END-->
 
-	}
-	function Footer()
-	{
-		//atur posisi 1.5 cm dari bawah
-		$this->SetY(-15);
-		//buat garis horizontal
-		//$this->Line(10,$this->GetY(),210,$this->GetY());
-		//Arial italic 9
-		$this->SetFont('Arial','I',9);
-        //        $this->Cell(0,10,'copyright gubugkoding.com Semarang ' . date('Y'),0,0,'L');
-		//nomor halaman
-		$this->Cell(0,10,'Halaman '.$this->PageNo().' dari {nb}',0,0,'R');
-	}
-}
-
-$pdf = new PDF();
-$pdf->AliasNbPages();
-$pdf->AddPage('L');
-$pdf->Content($laporan);
-$pdf->Output();
